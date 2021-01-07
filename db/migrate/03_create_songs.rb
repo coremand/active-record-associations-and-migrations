f3f44aebdd531adef63037e000a09a74479f5820
coremand <valinevite@gmail.com>
2021-01-06 19:49:53 -0700

    first commit

--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -1,2 +1,13 @@
 class CreateSongs < ActiveRecord::Migration[4.2]
+#Song belongs to an artist and genre, therefore should have a table
+#with artist_id and genre_id respectively
+# This table will be the join table because Artist has many genre through songs
+# and genre has many artist through songs
+    def change
+        create_table :songs do |t|
+          t.string :name
+          t.integer :artist_id
+          t.integer :genre_id
+        end
+      end
 end
